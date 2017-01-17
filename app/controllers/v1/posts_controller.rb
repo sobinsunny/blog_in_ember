dce08b9b42047f9c671a1a01f2ae00e2230e9b9e
Sobin <sobisunny@gmail.com>
2017-01-17 15:57:17 +0530

    Added Create Post

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -7,4 +7,13 @@ class V1::PostsController < ApplicationController
 		post = Post.find(params[:id])
 		render json: post
 	end
+	def create
+		post = Post.create(post_params)
+		render json: post
+	end
+	
+private
+	def post_params
+		 params.require(:post).permit(:title, :content)
+	end
 end
